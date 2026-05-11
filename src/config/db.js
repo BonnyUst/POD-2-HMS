7f2014e2697d16efcd819fc4436c91b34cfe3300
Keerthivaasan-Saravanan <308390@ust.com>
2026-05-11 21:51:21 +0530

    Revert "Keerthivaasan Project File Update"
    This reverts commit fd784de6bab3b3a472844f2e705f8c8e473bae76.

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-    if(mongoose.connection.readyState >= 1) return;
-
-    await mongoose.connect(ProcessingInstruction.env.MONGO_URI);
-};
-
-module.exports = connectDB;
--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyEmail, login, getMyInfo } = require('../controllers/auth.controller');
-const jwtAuth = require('../middlewares/jwtAuth.middleware');
-router.get('/verify', verifyEmail);
-router.post('/login', login);
-router.get('/me', jwtAuth, getMyInfo);
-module.exports = router;
